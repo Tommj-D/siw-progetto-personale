eaca7deb265543d6b1afae6ae35bb3d4d07b8f96
Tommj-D <tom.deangelis1@stud.uniroma3.it>
2026-06-23 10:39:11 +0200

    Aggiunto errore 403

--- siw-project/src/main/java/it/uniroma3/siw/controller/OrdineController.java
+++ siw-project/src/main/java/it/uniroma3/siw/controller/OrdineController.java
@@ -4,11 +4,13 @@ import it.uniroma3.siw.model.Utente;
 import it.uniroma3.siw.repository.UtenteRepository;
 import it.uniroma3.siw.service.OrdineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class OrdineController {
@@ -23,7 +25,15 @@ public class OrdineController {
     public String showOrders(
             @AuthenticationPrincipal UserDetails userDetails,
             Model model) {
+    	
+    	boolean isAdmin = userDetails.getAuthorities().stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN") || auth.getAuthority().equals("ADMIN"));
 
+        //L'admin non ha uno storico ordini
+        if (isAdmin) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Accesso negato: gli amministratori non hanno una pagina ordini.");
+        }
+        
         Utente utente = utenteRepository.findByEmail(userDetails.getUsername())
                 .orElseThrow();
 
